8e6e17e2403e46ace34d998d57a3898ef82b9412
MYounas <younus.ubit56@gmail.com>
2016-11-30 20:14:16 +0500

    43 done

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,42 +1,21 @@
 import {Component} from 'angular2/core';
-import {TwitterComponent} from './twitter.component';
 
 @Component({
     selector: 'my-app',
     template: `
-        <div *ngFor="#tweet of tweets">
-            <twitter [n]="tweet.n" [hit]="tweet.hit" [ImgSrc]="tweet.ImgSrc" [name]="tweet.name" [tName]="tweet.tName" [des]="tweet.des"></twitter>
+        <ul class="nav nav-pills">
+            <li [class.active]="viewMode=='map'"><a (click)="viewMode='map'">Map View</a></li>
+            <li [class.active]="viewMode=='list'"><a (click)="viewMode='list'">List View</a></li>
+        </ul>
+        <div [ngSwitch]="viewMode">
+            <template [ngSwitchWhen]="'map'" ngSwitchDefault>Map view content</template>
+            <template [ngSwitchWhen]="'list'">List view content</template>
         </div>
     `,
-    directives:[TwitterComponent]
 })
 export class AppComponent { 
- 
-    tweets=[
-        {
-            ImgSrc:"http://placeimg.com/100/100/people?1",
-            name:"Windward",
-            tName:"windwardstudio",
-            des:"Looking for a better company reporting or docgen app?",
-            n:5,
-            hit:false,
-        },
-        {
-            ImgSrc:"http://placeimg.com/100/100/people?2",
-            name:"AngularJS News",
-            tName:"angularjs_news",
-            des:"Right Relevance:Influencers,Articles and Conversations",
-            n:1,
-            hit:false,
-        },
-        {
-            ImgSrc:"http://placeimg.com/100/100/people?3",
-            name:"UX & Bootstrap",
-            tName:"3rdwave",
-            des:"10 Reasons Why Web Projects Fail",
-            n:10,
-            hit:true,
-        },
-    ];
-        
+    
+    viewMode='map';
+
 }
+
