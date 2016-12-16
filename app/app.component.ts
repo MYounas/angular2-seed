763adc27ccb48b075e0aab2a0367507a5ca57083
MYounas <younus.ubit56@gmail.com>
2016-12-16 01:26:41 -0800

    63 don

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,12 @@
 import {Component} from 'angular2/core';
-import {ZippyComponent} from './zippy.component';
+import {ContactFormComponent} from './contact-form.component'
 
 @Component({
     selector: 'my-app',
     template: `
-        <zippy title="1st title">
-            My name is younas
-        </zippy>
-        <zippy title="second title">
-            SOftware engineer
-        </zippy>
+        <contact-form></contact-form>
     `,
-    directives:[ZippyComponent]
+    directives:[ContactFormComponent]
 })
 export class AppComponent {
      
